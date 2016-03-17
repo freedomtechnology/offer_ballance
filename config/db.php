0a844b47b20e6f39818e98c73cd30b168fd832a9
marko <markosoft2000@yandex.ru>
2016-03-17 11:03:26 +0300

    yii basic template

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=ofer_invest',
+    'username' => 'root',
+    'password' => '230023',
+    'charset' => 'utf8',
+];
